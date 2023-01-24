80152a2a620a3e0b404f8e7fc0120b53104f6efb
jardinerolazarte <jardineriageneral2021@gmail.com>
2023-01-24 14:17:47 -0300

    Primer Commit

---
+++ js/main.js
@@ -0,0 +1,96 @@
+const combosDisponibles = document.getElementById("catalogo-combos");
+const carritoContenedor = document.getElementById("contenedor-carrito");
+const contadorCarrito = document.getElementById("contador-carrito");
+const totalCompra = document.getElementById("total-dinero");
+const vaciarCarrito = document.getElementById("vaciar-carrito");
+
+vaciarCarrito.addEventListener("click", () => {
+    carrito.length = 0
+    actualizarCarrito()
+})
+
+let carrito = [];
+
+combosDisponibles.addEventListener("click", (e) => {
+    if (e.target.classList.contains("agregar")){
+        validarComboAlCarrito(e.target.id)
+    };
+})
+
+const validarComboAlCarrito = (comboId) => {
+    const productoRepetido = carrito.find((combo) => combo.id == comboId)
+
+    if (!productoRepetido) { //undefine => false
+        const combo = combos.find(combo => combo.id == comboId)
+        agregarAlCarrito(combo.id)
+    } else {
+        productoRepetido.cantidad++
+        actualizarCarrito()
+    }
+}
+
+combos.forEach((combo) => {
+    const div = document.createElement("div");
+    div.classList.add("dis-flex");
+    div.innerHTML = `
+        <div class="card tarjeta">
+            <img src="${combo.img}" class="card-img-top catalogo-img" alt="Imagenes">
+                <div class="card-body">
+                  <h5 class="card-title">${combo.nombre}</h5>
+                  <p class="card-text">${combo.descripcion}</p>
+                    <div class="d-grid gap-2">
+                        <button id="${combo.id}" class="btn btn-danger agregar" type="button">Agregar al carrito <i class="fa-solid fa-cart-plus"></i></button>
+                    </div>
+                </div>
+        </div>
+    `
+    combosDisponibles.appendChild(div);
+})
+
+const actualizarCarrito = () => {
+    carritoContenedor.innerHTML= "";
+    
+    carrito.forEach((combo) => {
+        const div = document.createElement("div");
+        div.classList.add("dis-flex");
+        div.innerHTML = `
+        <p class="font-monospace">Combo: ${combo.nombre}</p>
+        <span class="font-monospace">Precio: ${combo.precio}</span>
+        <p class="font-monospace">Cantidad: ${combo.cantidad}</p>
+        <button type="button" class="btn btn-danger" onclick="eliminarDelCarrito(${combo.id})">Delete <i class="fa-solid fa-delete-left"></i></button>
+        `
+        carritoContenedor.appendChild(div)
+    })
+    contadorCarrito.innerHTML = carrito.length;
+    totalCompra.innerHTML = carrito.reduce((acc, combo) => acc + (combo.precio * combo.cantidad), 0)
+    guardarCarritoStorage(carrito)
+}
+
+const agregarAlCarrito = (comboId) =>{
+    const item = combos.find((combo) => combo.id === comboId);
+    carrito.push(item);
+    actualizarCarrito()
+}
+
+const eliminarDelCarrito = (comboEliminar) => {
+    const item = carrito.find((combo) => combo.id === comboEliminar);
+    const indice = carrito.indexOf(item)
+    carrito.splice(indice,1)
+    actualizarCarrito()
+}
+
+const guardarCarritoStorage = (carrito) => {
+    localStorage.setItem("carrito", JSON.stringify(carrito))
+};
+
+const obtenerCarritoStorage = () => {
+    const carritoStorage = JSON.parse(localStorage.getItem("carrito"))
+    return carritoStorage
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    if (localStorage.getItem("carrito")){
+        carrito = obtenerCarritoStorage()
+        actualizarCarrito()  
+    }
+})
---
+++ js/stock.js
@@ -0,0 +1,36 @@
+// Combos disponibles
+
+const combos = [
+    {
+        id: 1,
+        nombre: "Bucket",
+        precio: 1500,
+        cantidad:1,
+        img: "img/bucket.jpg",
+        descripcion: "8 PIEZAS DE POLLO. ELEGÍ NUESTRA TRADICIONAL RECETA ORIGINAL Y/O LA CRUJIENTE RECETA CRISPY"
+    },
+    {
+        id: 2,
+        nombre: "Box",
+        precio: 1200,
+        cantidad:1,
+        img: "img/box.jpg",
+        descripcion: "1 SUPER CRUNCH, 1 POPCORN SHAKER CHICO + SALSA, 2 ALITAS. INCLUYE PAPAS Y GASEOSA MEDIANAS."
+    },
+    {
+        id: 3,
+        nombre: "Sandwitch",
+        precio: 1000,
+        cantidad:1,
+        img: "img/sandwitch.jpg",
+        descripcion: "SANDWICH DE POLLO CRISPY CON LECHUGA, TOMATE, BACON, SALSA BBQ, QUESO Y PAN BRIOCHE."
+    },
+    {
+        id: 4,
+        nombre: "Postre",
+        precio: 800,
+        cantidad:1,
+        img: "img/postres.jpg",
+        descripcion: "TURRÓN CON MANÍ O CHOCOLATES, COMBINADA CON TU SALSA FAVORITA DE DULCE DE LECHE, CHOCOLATE O FRUTILLA."
+    },
+]
